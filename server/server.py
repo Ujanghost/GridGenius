919a7b006f8196195f3483d274c34c32b8dcaee6
Ujan Galui <ujan.g570@gmail.com>
2024-08-27 08:29:56 +0530

    implemented vite

---
+++ server/server.py
@@ -0,0 +1,22 @@
+from flask import flask, jsonify
+
+
+app=flask(__name__)
+
+@app.route("/api/users",methods=['GET'])
+
+def users():
+    return jsonify(
+        {
+            "users":[
+                'arpan',
+                'zach',
+                'jessie'
+                
+            ]
+        }
+    )
+
+
+
+if __name__ == "__main__"
